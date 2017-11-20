88cfc6f13b4dd8d062454dea825e249e6a622aa6
sandstranger <sandstranger@gmail.com>
2017-11-20 21:33:23 +0400

    move prebuilt trash to vendor

--- s3_h560-vendor-blobs.mk
+++ s3_h560-vendor-blobs.mk
@@ -2,8 +2,6 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib,s
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/lib64,system/lib64)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/etc,system/etc)
-
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/usr,system/usr)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/jiayu/s3_h560/xbin,system/xbin)
